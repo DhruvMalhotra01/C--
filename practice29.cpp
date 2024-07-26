9876f8b6c87cd0beb11f37d11013d59a8509fc8c
DhruvMalhotra01 <malhotradhruv112004@gmail.com>
2024-07-26 14:42:20 +0530

    done

---
+++ practice29.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class Animal{
+    public:
+        void eat(){
+            cout<<"Eating.."<<endl;
+        }
+};
+class Dog: public Animal{
+    public:
+        void eat(){
+            cout<<"Eating bread"<<endl;
+        }
+};
+int main(void){
+    Dog d = Dog();
+    d.eat();
+    d.Animal::eat();
+    return 0;
+}
