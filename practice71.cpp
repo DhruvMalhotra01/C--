fea63a7eafee5a354cd856f21339c1e575e6341f
Dhruv Malhotra <malhotradhruv112004@gmail.com>
2024-10-22 22:45:38 +0530

    Add files via upload

---
+++ practice71.cpp
@@ -0,0 +1,26 @@
+#include<algorithm>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> number ={ 3,1,4,1,5,9};
+
+    sort(number.begin().number.end());
+
+    int max_element  = 'max_element(number.begin(),number.end());'
+
+    int count_ones = count(number.begin(),number.end(),1);
+
+    cout<<"Sorted numbers:";
+    for(int num : numbers){
+        cout<<num<<" ";
+
+    }
+    cout<<endl;
+
+    cout<<"Maximum element:"<<max_element<<endl;
+    cout<<"count of 1s"<<count_onces<<endl;
+    return 0;
+    
+}
